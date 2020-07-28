e02c25f56bac27457f64fd5b1b5c05860f2cd49b
muhyulianto <myulianto72@gmail.com>
2020-07-28 15:18:36 +0700

    update tampilan show

--- resources/views/artikel/show.blade.php
+++ resources/views/artikel/show.blade.php
@@ -8,17 +8,29 @@
                     <div class="card-header">
                         Info detail artikel
                     </div>
-                    <div class="card-body d-flex">
-                        <div class="w-50 px-3">
+                    <div class="card-body d-flex flex-column">
+                        <div class="pb-3">
                             <img src="{{ asset('storage/'.$artikel->gambar) }}" class="img-fluid" alt="">
                         </div>
                         <table class="table">
                             <tr>
-                                <th>
+                                <th class="w-25">
                                     Judul artikel
                                 </th>
                                 <td>
                                     {{ $artikel->judul }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>
+                                    Kategori artikel
+                                </th>
+                                <td>
+                                    @forelse ($artikel->kategoris as $kategori)
+                                        {{ $kategori->nama . "," }}
+                                    @empty
+                                        Uncategorized
+                                    @endforelse
                                 </td>
                             </tr>
                             <tr>
